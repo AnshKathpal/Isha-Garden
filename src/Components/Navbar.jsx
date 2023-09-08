0c1f74b6bdf25ce8230e633d98284208241987d6
AnshKathpal <storage.2.3107@gmail.com>
2023-09-09 03:10:09 +0530

    homepage

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,79 @@
-import React from 'react'
-import {Box} from "@chakra-ui/react"
+import React from "react";
+import { Box, Flex } from "@chakra-ui/react";
+import logo from "../Images/logo.png";
+
+import { VscHeart } from "react-icons/vsc";
 
 export const Navbar = () => {
   return (
- <>
- 
- <Box border = "1px solid green" bg = "white" h = "15vh">
-    
- </Box>
+    <>
+      <Flex
+        justify="center"
+        alignItems="flex-end"
+        border="1px solid green"
+        bg="white"
+        h="10vh"
+        gap="40"
+      >
+        <Flex direction={"column"}
+        border={"1px solid black"}
+        sx={{
+          "&::before": {
+            content: '""',
+            opacity: "0",
+          },
+          "&:hover::before": {
+            opacity: "1",
+          },
+        }}
+        >
+            <Box>
+          Our Story
+            </Box>
+        </Flex>
 
- </>
-  )
-}
+        <Flex direction={"column"}
+        border={"1px solid black"}
+          sx={{
+            "&::before": {
+              content: '"check"',
+              opacity: "0",
+            },
+            "&:hover::before": {
+              opacity: "1",
+            },
+          }}
+        >Weddings</Flex>
+
+        <Box>Isha Garden</Box>
+
+        <Flex direction={"column"}
+        border={"1px solid black"}
+          sx={{
+            "&::before": {
+              content: '"check"',
+              opacity: "0",
+              transition : " opacity 0.3s"
+            },
+            "&:hover::before": {
+              opacity: "1",
+            },
+          }}
+        >Gallery</Flex>
+
+<Flex direction={"column"}
+        border={"1px solid black"}
+          sx={{
+            "&::before": {
+              content: '"check"',
+              opacity: "0",
+            },
+            "&:hover::before": {
+              opacity: "1",
+            },
+          }}
+        >Contact</Flex>
+      </Flex>
+    </>
+  );
+};
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,17 +1,64 @@
 import React from "react";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Text, Grid, GridItem } from "@chakra-ui/react";
 import { Navbar } from "../Components/Navbar";
+import headerImage from "../Images/image-header.jpg";
+import coupleImage from "../Images/couple.jpg";
 
 export const HomePage = () => {
   return (
     <div>
-      <Box border="1px solid red" h="85vh" bg="pink"></Box>
-      <div style={{position : "sticky" , top : 0, zIndex : "100" }} >
+      <Box h="90vh">
+        <img
+          src={headerImage}
+          style={{
+            objectFit: "cover",
+            width: "100%",
+            height: "100%",
+            filter: "brightness(60%)",
+          }}
+          alt=""
+        />
+      </Box>
+      <div style={{ position: "sticky", top: 0, zIndex: "100" }}>
         <Navbar />
       </div>
 
-      <Box border="1px solid red" h="85vh" bg="blue"></Box>
-      <Box border="1px solid red" bg="green" h="85vh"></Box>
+      <Flex
+        width="97%"
+        margin={"auto"}
+        h="80vh"
+        bgImage={headerImage}
+        justify={"center"}
+        align="center"
+      >
+        <Box w="70%" h="50%" bg="white">
+          <Text></Text>
+        </Box>
+      </Flex>
+      <Flex width="97%" margin={"auto"} h="90vh">
+        <Box width="50%" border="1px solid red" overflow={"hidden"}>
+          <img src={coupleImage} alt="" />
+        </Box>
+
+        <Box width="50%" border="1px solid red"></Box>
+      </Flex>
+
+      <Grid width="97%" margin={"auto"} gridTemplateColumns="repeat(2,1fr)">
+        <GridItem h="50vh" border="1px solid blue"></GridItem>
+        <GridItem h="50vh" border="1px solid blue">
+          <img
+            src="https://images.pexels.com/photos/10172663/pexels-photo-10172663.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt=""
+            style={{ objectFit: "cover", height: "100%", width: "100%" }}
+          />
+        </GridItem>
+        <GridItem h="50vh" border="1px solid pink" overflow={"hidden"}>
+          <img src="https://images.pexels.com/photos/587741/pexels-photo-587741.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        </GridItem>
+        <GridItem h="50vh" border="1px solid pink"></GridItem>
+        <GridItem h="50vh" border="1px solid green"></GridItem>
+        <GridItem h="50vh" border="1px solid green"></GridItem>
+      </Grid>
     </div>
   );
 };
